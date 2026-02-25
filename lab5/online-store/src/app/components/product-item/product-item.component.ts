9a4b709f544b843dce33e66bcb3e5c8e4cf8b19e
IguessnotHehehaha <tnurikhanov@gmail.com>
2026-02-25 09:53:01 +0500

    lab5 changes

--- lab5/online-store/src/app/components/product-item/product-item.component.ts
+++ lab5/online-store/src/app/components/product-item/product-item.component.ts
@@ -1,17 +1,40 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { DecimalPipe } from '@angular/common';
+import { DecimalPipe, NgFor } from '@angular/common';
 import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [DecimalPipe],
+  imports: [NgFor, DecimalPipe],
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css'],
 })
 export class ProductItemComponent {
   @Input({ required: true }) product!: Product;
   @Output() deleteProduct = new EventEmitter<number>();
+
+  selectedIndex: number = 0;
+
+  get selectedImage(): string {
+    return this.product.images[this.selectedIndex] ?? this.product.image;
+  }
+
+  selectImage(index: number): void {
+    this.selectedIndex = index;
+  }
+
+  prevImage(): void {
+    this.selectedIndex =
+      (this.selectedIndex - 1 + this.product.images.length) % this.product.images.length;
+  }
+
+  nextImage(): void {
+    this.selectedIndex = (this.selectedIndex + 1) % this.product.images.length;
+  }
+
+  getStars(): boolean[] {
+    return Array.from({ length: 5 }, (_, i) => i < Math.floor(this.product.rating));
+  }
 
   like(): void {
     this.product.likes += 1;
